c404aa380540e60ed686c4ab6fc2bc5874529841
ThuanEnvity <thuanit.mta@gmail.com>
2019-11-15 17:30:08 +0700

    add schema message

--- app/database/schemas/messege.js
+++ app/database/schemas/messege.js
@@ -5,11 +5,14 @@
  * @date 2019/11/15
  */
 
- var Mongoose = require('mongoose');
+var Mongoose = require('mongoose');
 
- var message = new Mongoose.Schema({
-     created_by: {type: String, required: true},
-     to_username: {type: String},
-     message: {type: String},
-     is_viewed 
- });
+var messageSchema = new Mongoose.Schema({
+    created_by: { type: String, required: true },
+    to_username: { type: String },
+    message: { type: String, required: true },
+    is_viewed: { type: Boolean }
+});
+
+var messageModel = Mongoose.model('message', messageSchema);
+module.exports = messageModel;
